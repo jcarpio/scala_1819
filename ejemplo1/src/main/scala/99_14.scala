7f61f97372c35a4022ff7fe5d3c1c53f5c9f363c
jcarpio <jose.carpio@gmail.com>
2019-01-08 15:47:36 +0100

    "99_14"

---
+++ ejemplo1/src/main/scala/99_14.scala
@@ -0,0 +1,14 @@
+// P14 (*) Duplicate the elements of a list.
+//     Example:
+//     scala> duplicate(List('a, 'b, 'c, 'c, 'd))
+//     res0: List[Symbol] = List('a, 'a, 'b, 'b, 'c, 'c, 'c, 'c, 'd, 'd)
+
+object P14 {
+  def duplicate[A](ls: List[A]): List[A] = ls flatMap { e => List(e, e) }
+
+
+  def main(args: Array[String]) {
+    print(duplicate(List('a, 'b, 'c, 'c, 'd)));
+    print("\n");
+  }
+}
