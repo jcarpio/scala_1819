ee04689e0dab57ba4980777ebdf482e542f624ca
jcarpio <jose.carpio@gmail.com>
2019-01-08 15:49:53 +0100

    "99_15"

---
+++ ejemplo1/src/main/scala/99_15.scala
@@ -0,0 +1,15 @@
+// P15 (**) Duplicate the elements of a list a given number of times.
+//     Example:
+//     scala> duplicateN(3, List('a, 'b, 'c, 'c, 'd))
+//     res0: List[Symbol] = List('a, 'a, 'a, 'b, 'b, 'b, 'c, 'c, 'c, 'c, 'c, 'c, 'd, 'd, 'd)
+
+object P15 {
+  def duplicateN[A](n: Int, ls: List[A]): List[A] =
+    ls flatMap { List.fill(n)(_) }
+
+
+  def main(args: Array[String]) {
+    print(duplicateN(3, List('a, 'b, 'c, 'c, 'd)));
+    print("\n");
+  }
+}
